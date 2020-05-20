c6f75226b1a84516c51da0730263a9c0cb408288
Alexey Altunin <alexey.altunin@frontiercargroup.com>
2020-05-20 23:11:24 +0300

    add solution for 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,21 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.bufferString = '';
   }
 
   _transform(chunk, encoding, callback) {
+    this.bufferString += chunk;
+    callback();
   }
 
   _flush(callback) {
+    for (const chunkPart of this.bufferString.split(os.EOL)) {
+      this.push(chunkPart);
+    }
+
+    callback();
   }
 }
 
