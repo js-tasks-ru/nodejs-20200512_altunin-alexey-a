920952ead538790b2947ebe00588fbef0e6ef9de
Alexey Altunin <alexey.altunin@frontiercargroup.com>
2020-05-24 20:24:28 +0300

    add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fse = require('fs-extra');
 
 const server = new http.Server();
 
@@ -11,9 +12,30 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+      if (req.url.split('/').length > 2) {
+        res.statusCode = 400;
+        res.end('nested directories are not supported');
+
+        break;
+      }
+
+      const readStream = fse.createReadStream(filepath);
+      readStream.pipe(res);
+
+      readStream.on('end', function(err) {
+        res.statusCode = 200;
+        fse.unlink(filepath);
+        res.end('file was removed');
+      });
+
+      readStream.on('error', function(err) {
+        if (err.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('file is not found');
+        }
+      });
 
       break;
-
     default:
       res.statusCode = 501;
       res.end('Not implemented');
