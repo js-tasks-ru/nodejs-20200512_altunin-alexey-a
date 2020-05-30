6c77136ce749a4f96e8524d501c46b85743ff48a
Alexey Altunin <alexey.altunin@frontiercargroup.com>
2020-05-30 17:19:40 +0300

    add solution for 6-module 2-task

--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,42 @@
+const Product = require('../models/Product');
+
+function mapProduct(product) {
+  return {
+    title: product.title,
+    id: product.id,
+    category: product.category,
+    subcategory: product.subcategory._id,
+    price: product.price,
+    description: product.description,
+    images: product.images,
+  };
+}
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  const subcaragoryId = ctx.url.split('subcategory=')[1];
+
+  if (subcaragoryId === undefined) {
+    return next();
+  }
+
+  const productList = await Product.find({"subcategory": subcaragoryId});
+
+  ctx.body = {products: productList.map(mapProduct)};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const productList = await Product.find();
+
+  ctx.body = {products: productList.map(mapProduct)};
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const product = await Product.findById(ctx.params.id);
+
+  if (product === null) {
+    ctx.throw(404, 'product not found');
+  }
+
+  ctx.body = {product: mapProduct(product)};
 };
 
