3e55befab9ab3f6ba3c881533672bdc3ca9b4937
Alexey Altunin <alexey.altunin@frontiercargroup.com>
2020-06-07 13:13:28 +0300

    add solution for 7-module 2-task

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,24 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) {
+    return done(null, false, 'Не указан email');
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      const newUser = new User({email, displayName});
+
+      await newUser.validate();
+      await newUser.save();
+
+      return done(null, newUser);
+    }
+
+    return done(null, user);
+  } catch (error) {
+    return done(error, false);
+  }
 };
