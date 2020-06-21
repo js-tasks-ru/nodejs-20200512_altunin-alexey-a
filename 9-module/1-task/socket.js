ca88c786a9bb9297f4846b322a8ce66c13a44299
Alexey Altunin <alexey.altunin@frontiercargroup.com>
2020-06-21 15:06:54 +0300

    add solution for 9-module 1-task

--- 9-module/1-task/socket.js
+++ 9-module/1-task/socket.js
@@ -10,8 +10,31 @@ function socket(server) {
     next();
   });
 
-  io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+  io.on('connection', async function(socket) {
+    const token = socket.handshake.query.token;
+
+    if (token) {
+      const session = await Session.findOne({token}).populate('user');
+      if (!session) {
+        ctx.throw(401, 'wrong or expired session token');
+      }
+
+      socket.user = session.user;
+
+      socket.on('message', async (msg) => {
+        const messageData = {
+          user: socket.user.displayName,
+          chat: socket.user.id,
+          text: msg,
+          date: Date.now(),
+        };
+
+        const message = new Message(messageData);
+        await message.save();
+      });
+    } else {
+      io.emit('error', "anonymous sessions are not allowed");
+    }
   });
 
   return io;
