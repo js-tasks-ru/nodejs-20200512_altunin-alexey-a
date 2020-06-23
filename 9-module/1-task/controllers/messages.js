24b980a6753593a8eff89d867b62fd8e31673d20
Alexey Altunin <alexey.altunin@frontiercargroup.com>
2020-06-23 17:07:46 +0300

    fix code review comments

--- 9-module/1-task/controllers/messages.js
+++ 9-module/1-task/controllers/messages.js
@@ -2,7 +2,7 @@ const Message = require('../models/Message');
 const mapMessage = require('../mappers/message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  if (ctx.req.headers.authorization) {
+  if (ctx.user) {
     const messages = await Message.find({user: ctx.user.displayName}).limit(20);;
 
     ctx.body = {messages: messages.map(mapMessage)};
--- 9-module/1-task/socket.js
+++ 9-module/1-task/socket.js
@@ -7,34 +7,36 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
-    next();
-  });
-
-  io.on('connection', async function(socket) {
     const token = socket.handshake.query.token;
 
     if (token) {
       const session = await Session.findOne({token}).populate('user');
       if (!session) {
-        ctx.throw(401, 'wrong or expired session token');
+        next(new Error('wrong or expired session token'));
       }
-
-      socket.user = session.user;
-
-      socket.on('message', async (msg) => {
-        const messageData = {
-          user: socket.user.displayName,
-          chat: socket.user.id,
-          text: msg,
-          date: Date.now(),
-        };
-
-        const message = new Message(messageData);
-        await message.save();
-      });
     } else {
-      io.emit('error', "anonymous sessions are not allowed");
+      next(new Error('anonymous sessions are not allowed'));
     }
+
+    next();
+  });
+
+  io.on('connection', async function(socket) {
+    const token = socket.handshake.query.token;
+    const session = await Session.findOne({token}).populate('user');
+    socket.user = session.user;
+
+    socket.on('message', async (msg) => {
+      const messageData = {
+        user: socket.user.displayName,
+        chat: socket.user.id,
+        text: msg,
+        date: Date.now(),
+      };
+
+      const message = new Message(messageData);
+      await message.save();
+    });
   });
 
   return io;
